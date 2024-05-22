bdc27b653d081cb31ab3782523130f6ae6474bcc
alw-bruh <122893972+alw-bruh@users.noreply.github.com>
2024-05-23 06:45:11 +0700

    Add files via upload

---
+++ php.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h6>Alw_bruh</h6>
+        </header>
+        
+    </body>
+
+
+
+</html>
